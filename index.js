103e2ae9f76365a46c32e74060ef8226467b104e
Justin Crabtree <justincrab@gmail.com>
2017-09-20 12:54:58 -0500

    Finishing Project
    it all happened in this comit, shit went DOOOOWN

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/* Program: Inquirer Pizza Order-er
+   Developer:   Justin Crabtree
+   Date:        9.20.17
+   Purpose:     Simple app for ordering pizzas.
+                User is asked a series of questions
+                then asked to confirm their order
+                which determines the output message */
 const chalk = require('chalk');
 var inquirer = require('inquirer');
 
@@ -6,13 +13,40 @@ let questions = [{
     name : "sizeAnswer",
     message : "What size of 'za?",
     default: "XXL",
-    choices : ["S", "M", "L", "XL", "XXL"], 
+    choices : ["S", "M", "L", "XL", "XXL"]
 },
 {
-    //second question object
+    type : "list",
+    name : "crustAnswer",
+    message : "What kind of crust you want brev?",
+    default: "Stuffed Crust",
+    choices : ["\nHand-Tossed", "Stuffed Crust", "Thin", "Chicago"]
+},
+{
+    type: "checkbox",
+    name: "toppingsAnswer",
+    message: "What kinda toppings u dig fam?",
+    default: "Cheese",
+    choices: ["Cheese ", "Pepperonni ", "Canadian Bacon ", "Sausage ", "Green Peppers ", "Shrooms", "Olives"]
+
 }];
-
-inquirer.prompt([questions]).then(function (answers) {
-
+// another question object had to be defined with another inquirer used below
+let question2 = [{
+    type: "confirm",
+    name: "confirm",
+    message: "Is this lookin good my G?",
+    default: "yes",
+}]
+console.log(chalk.red("Welcome to Cosmo Pizza Pizzagate Express\nYou will be asked a series of questions, \nuse your up/down arrows and enter to complete your order!"));
+inquirer.prompt(questions).then(function (answers) {
+    console.log(chalk.bgMagenta("Size: "+answers.sizeAnswer));
+    console.log(chalk.bgCyan("Crust: "+answers.crustAnswer));
+    console.log(chalk.bgGreen("Toppings: "+answers.toppingsAnswer));
+    //notice the tucked inquirer prompt here
+    inquirer.prompt(question2).then(function (answers2){
+        if (answers2.confirm === true)
+            console.log("It's on it's way!");
+        else
+            console.log("Sorry for the inconvenience, honey boo boo");
+    });
 });
-console.log(inquirer.prompt([questions]));
